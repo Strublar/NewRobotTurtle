820a0fdded26806c6c1651e03bf8cbe3b043e28f
Strublar <daluzmatteo@gmail.com>
2020-01-06 10:39:00 +0100

    Add files via upload

--- Mousy.java
+++ Mousy.java
@@ -12,12 +12,14 @@ public class Mousy implements MouseListener{
   // Fields
   //
   private Window window;
+  private Game game;
   
   //
   // Constructors
   //
-  public Mousy (Window newWindow) {
+  public Mousy (Window newWindow, Game newGame) {
     this.window = newWindow;
+    this.game = newGame;
   };
   
   //
@@ -29,19 +31,7 @@ public class Mousy implements MouseListener{
      */
     public void mouseClicked(MouseEvent e){
         //System.out.println("mouse clicked at x:"+e.getX()+" y:"+e.getY());
-    String state = this.window.getWindowState();
-    if(state.equals("Menu"))
-    {
-      //Faire les tests relatifs aux menus
-    }
-    else if(state.equals("Choice"))
-    {
-      //Faire les tests relatifs au choix de l'action du tour du joueur
-    }
-    else if(state.equals("Turn"))
-    {
-      //Faire les tests relatifs au tour du jeu (choisir carte/clic sur le terrain
-    }
+
     }
   
     /**
@@ -50,6 +40,44 @@ public class Mousy implements MouseListener{
      */
     public void mousePressed(MouseEvent e){
         //System.out.println("mouse pressed at x:"+e.getX()+" y:"+e.getY());
+      String state = this.window.getWindowState();
+      if(state.equals("Menu"))
+      {
+        //Faire les tests relatifs aux menus
+      }
+      else if(state.equals("Choice"))
+      {
+        //Faire les tests relatifs au choix de l'action du tour du joueur
+      }
+      else if(state.equals("Turn"))
+      {
+        //Faire les tests relatifs au tour du jeu (choisir carte/clic sur le terrain
+        if(this.game.getPlayers()[this.game.getCurrentPlayer()].getPlayerState().equals("AddCard"))
+        {
+          int offsetX = 820+12;
+          int length = 135;
+          int betweenX = 4;
+
+          int offsetY =  146 + 200 + 51;
+          int height = 200;
+          int betweenY = 200+51;
+          for(int i=0;i<5;i++)
+          {
+            if(e.getX()>= offsetX + i*(length+betweenX) && e.getX()<= offsetX + length + i*(length+betweenX))
+            {
+              if(e.getY()>= offsetY && e.getY()<= offsetY+height)
+              {
+                //Clic sur une carte à ajouter
+
+              }
+              else if(e.getY()>= offsetY+betweenY && e.getY()<= offsetY+height+betweenY)
+              {
+                //Clic de validation
+              }
+            }
+          }
+        }
+      }
     }
     
     
